bbb239e60164785685ec37b4e07502c3079dbacd
OlgaMinina <arb.test2020@gmail.com>
2023-04-08 02:28:15 +0200

    branch2 - commit 4 - total 7

--- RedRover_06/src/main/java/org/example/Main.java
+++ RedRover_06/src/main/java/org/example/Main.java
@@ -3,7 +3,10 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+
         test_2.print();
+
+        System.out.println("Everything working fine");
 
 
     }
